51910ce2457554f5f487715a211d1f425e439626
priyanshi09 <ps3369@nyu.edu>
2017-04-02 18:14:26 -0400

    Week 1 material

---
+++ Week 3/my_calc.py
@@ -0,0 +1,12 @@
+# Create a file by name my_calc.py in your current directory and write this code:
+def mul(a ,b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
